d6ac270427e52f3dca87d97fc8182feefdf09715
ddiebold <68356173+ddiebold@users.noreply.github.com>
2020-07-15 16:14:59 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Welcome to Fit Heart, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome to Fit Heart, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }  
